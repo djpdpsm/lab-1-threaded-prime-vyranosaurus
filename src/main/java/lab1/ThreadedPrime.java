c625d2a3aacf04e4247d585f309691e06f079645
vyranosaurus <155831670+vyranosaurus@users.noreply.github.com>
2024-03-15 23:15:11 +0800

    everything basically

--- src/main/java/lab1/ThreadedPrime.java
+++ src/main/java/lab1/ThreadedPrime.java
@@ -4,18 +4,64 @@ import java.util.*;
 
 public class ThreadedPrime {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
-
         ThreadedPrime threadedPrime = new ThreadedPrime();
-        List<Integer> primes = threadedPrime.getListOfPrimes(0,null);
+        List<Integer> primes = threadedPrime.getListOfPrimes(4,100);
         threadedPrime.printListOfPrimes(primes);
     }
 
     private List<Integer> getListOfPrimes(int numberOfThreads, Integer maxNumber) {
+        int elementsPerThread = maxNumber/numberOfThreads;
+        //long now = System.currentTimeMillis(); // I used this to measure speed, ignore. -Nathan
+        List<Integer> primeList = new ArrayList<>();
+        List<Thread> threads = new ArrayList<>();
 
-        return null;
+        for (int i = 0; i < numberOfThreads; i++) {
+            int listEnd = (elementsPerThread)*(i+1);
+            int counter = i;
+            Thread newThread = new Thread(() -> {
+                for (int x = 2+(elementsPerThread*counter); x < listEnd; x++){
+
+                    int flag = 0;
+                    int upperBound = x/2;
+
+                    for (int y = 2; y <= upperBound; y++){
+                        if(x% y == 0) {
+                            flag = 1;
+                            break;
+                        }
+                    }
+
+                    if(flag == 0) {
+                        primeList.add(x);
+                    }
+                }
+            });
+            threads.add(newThread);
+            newThread.start();
+
+        }
+
+        for (Thread thread : threads) {
+            try {
+                thread.join();
+            } catch (InterruptedException e) {
+                e.printStackTrace();
+            }
+        }
+        //System.out.println((System.currentTimeMillis() - now)); // measuring runtime lol
+        return new ArrayList<>(primeList);
     }
 
     private void printListOfPrimes(List<Integer> primes) {
-
+        Collections.sort(primes);
+        int counter = 1;
+        for (Integer element : primes) {
+            String suffix = ", ";
+            if (counter==10){
+                suffix = ", \n";
+                counter = 0;
+            }
+            System.out.print(element+suffix);
+            counter++;
+        }
     }}
